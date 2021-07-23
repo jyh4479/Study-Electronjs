2fd370bce3a51a2ddb13d23eb33ada7ccd7c2c81
Jeong yong hoon <jyh.dev@gmail.com>
2021-07-23 17:13:47 +0900

    Update PopUp Component
    Component 스켈레톤 구현 --> 반응형으로 변경과 더불어 전체 Component refactoring진행하기

--- electron-react-app-test/src/components/PopUp.js
+++ electron-react-app-test/src/components/PopUp.js
@@ -5,18 +5,23 @@ import {CallButtonBox} from "./index";
 const PopUp = props => {
     return (
         <div>
-            <div className={'title'}>title</div>
-
             <div className={'box'}>
+                <div className={'title'}>title</div>
+                <div className={'info-box'}>
+                    <div className={'img-box'}>
+                        <img src="img/icon-profile-80-px-default.png"
+                             alt="profile"
+                             srcSet="img/icon-profile-80-px-default@2x.png 2x,
+                        img/icon-profile-80-px-default@3x.png 3x"/>
+                    </div>
 
-                <img src="img/icon-profile-80-px-default.png"
-                     alt="profile"
-                     srcSet="img/icon-profile-80-px-default@2x.png 2x,
-                        img/icon-profile-80-px-default@3x.png 3x"
-                     className="info-img">
-                </img>
+                    <div className={'text-box'}>
+                        <text>HIHI</text>
+                    </div>
 
-                <div className={'container'}>
+                </div>
+
+                <div>
                     <div className={'row key-c'}>
                         <CallButtonBox pageType={'c'}></CallButtonBox>
                     </div>
